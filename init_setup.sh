c7db80681078381d8f556a5be6729bfdd5759bff
Suraj Verma <stdmail2552@gmail.com>
2023-05-15 19:26:26 +0530

    Updated configs and requirements

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START" 
 echo [$(date)]: "creating env with python 3.8 version" 
-conda create --prefix ./env python=3.8 -y
+conda create --prefix ./phish_predictor_env python=3.8 -y
 echo [$(date)]: "activating the environment" 
-source activate ./env
+source activate ./phish_predictor_env
 echo [$(date)]: "installing the dev requirements" 
 pip install -r requirements_dev.txt
 echo [$(date)]: "END" 
