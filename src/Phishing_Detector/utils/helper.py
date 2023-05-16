5cbf2f14022b3cfd2dd0f9ddec3d0cbb10c77da4
Suraj Verma <stdmail2552@gmail.com>
2023-05-16 20:09:48 +0530

    Helper Functions Updated

--- src/Phishing_Detector/utils/helper.py
+++ src/Phishing_Detector/utils/helper.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from DeepCNNClassifer import logger
+from Phishing_Detector import logger
 import json
 import joblib
 from ensure import ensure_annotations
@@ -24,7 +24,6 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
@@ -41,8 +40,6 @@ def create_directories(path_to_directories: list, verbose=True):
     """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
-        if verbose:
-            logger.info(f"created directory at: {path}")
 
 
 @ensure_annotations
@@ -55,7 +52,6 @@ def save_json(path: Path, data: dict):
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
-    logger.info(f"json file saved at: {path}")
 
 
 @ensure_annotations
@@ -69,7 +65,6 @@ def load_json(path: Path) -> ConfigBox:
     with open(path) as f:
         content = json.load(f)
 
-    logger.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
 
 
@@ -81,8 +76,6 @@ def save_bin(data: Any, path: Path):
         path (Path): path to binary file
     """
     joblib.dump(value=data, filename=path)
-    logger.info(f"binary file saved at: {path}")
-
 
 @ensure_annotations
 def load_bin(path: Path) -> Any:
@@ -93,7 +86,6 @@ def load_bin(path: Path) -> Any:
         Any: object stored in the file
     """
     data = joblib.load(path)
-    logger.info(f"binary file loaded from: {path}")
     return data
 
 
