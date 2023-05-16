9d51127186a4b46bd7bf62ef649fb41aaf1235f2
Suraj Verma <stdmail2552@gmail.com>
2023-05-16 20:15:17 +0530

    Config Entity Updated For Stage - 1 (Data Ingestion)

---
+++ src/Phishing_Detector/entity/config_entity.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir : Path
+    source_url : str
+    local_data_dir_path : Path
+    processed_data_dir_path : Path
+    local_data_file_path : Path
+    processed_data_file_path : Path
