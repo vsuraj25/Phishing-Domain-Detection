ba6bb2859b58625c9f1d4d482d9b101b67be31a5
Suraj Verma <stdmail2552@gmail.com>
2023-05-16 20:21:51 +0530

    Stage-1(Data Ingestion) Completed Successfully

---
+++ src/Phishing_Detector/pipeline/stage_1.py
@@ -0,0 +1,18 @@
+from Phishing_Detector.components import DataIngestion
+from Phishing_Detector.config import ConfigurationManager
+from Phishing_Detector import logger
+
+
+def main():
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config = data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.convert_to_csv()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        raise e
