8b85eed575f5b40ae523d0855bd120c62bfaebf4
Suraj Verma <stdmail2552@gmail.com>
2023-05-17 14:36:00 +0530

    Stage-3(Data Transformation) Completed Successfully

---
+++ src/Phishing_Detector/pipeline/stage_3.py
@@ -0,0 +1,16 @@
+from Phishing_Detector.components import DataTransformation
+from Phishing_Detector.config import ConfigurationManager
+
+
+def main():
+    config = ConfigurationManager()
+    data_transformation_config = config.get_data_transformation_config()
+    data_transformation = DataTransformation(config = data_transformation_config)
+    data_transformation.transform_data()
+    data_transformation.split_data()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        raise e
