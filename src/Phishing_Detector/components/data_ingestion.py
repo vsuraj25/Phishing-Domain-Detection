0e1ec4feaf17aea6c8e989b49b5294d7265f602d
Suraj Verma <stdmail2552@gmail.com>
2023-05-20 20:44:35 +0530

    Document Formatted

--- src/Phishing_Detector/components/data_ingestion.py
+++ src/Phishing_Detector/components/data_ingestion.py
@@ -8,8 +8,7 @@ import pandas as pd
 
 
 class DataIngestion:
-    
-    def __init__(self, config:DataIngestionConfig):
+    def __init__(self, config: DataIngestionConfig):
         logger.info(f"{'>'*10} Starting Data Ingestion Stage! {'<'*10}")
         logger.info(f"Getting Configuration for Data Ingestion Stage...")
 
@@ -23,14 +22,20 @@ class DataIngestion:
     def download_file(self):
         try:
             if not os.path.exists(self.config.local_data_file_path):
-                logger.info(f"Downloading source file from url{self.config.source_url} ...")
-                request.urlretrieve(
-                    url = self.config.source_url,
-                    filename= self.config.local_data_file_path
+                logger.info(
+                    f"Downloading source file from url{self.config.source_url} ..."
                 )
-                logger.info(f"File downloaded and saved to {self.config.local_data_file_path} ...")
+                request.urlretrieve(
+                    url=self.config.source_url,
+                    filename=self.config.local_data_file_path,
+                )
+                logger.info(
+                    f"File downloaded and saved to {self.config.local_data_file_path} ..."
+                )
             else:
-                logger.info(f"File already present at path {self.config.local_data_file_path}")
+                logger.info(
+                    f"File already present at path {self.config.local_data_file_path}"
+                )
         except Exception as e:
             raise e
 
@@ -39,14 +44,22 @@ class DataIngestion:
             if not os.path.exists(self.config.processed_data_file_path):
                 logger.info(f"Converting the downloaded arff file into csv format...")
                 data = arff.loadarff(Path(self.config.local_data_file_path))
-                data= pd.DataFrame(data[0])
-                data = data.apply(lambda x: x.str.decode('utf8'))
-                data = data.apply(pd.to_numeric, errors = 'ignore')
-                data.to_csv(path_or_buf= self.config.processed_data_file_path, index=False)
-                logger.info(f"File converted to csv and saved at {self.config.processed_data_file_path}")
+                data = pd.DataFrame(data[0])
+                data = data.apply(lambda x: x.str.decode("utf8"))
+                data = data.apply(pd.to_numeric, errors="ignore")
+                data.to_csv(
+                    path_or_buf=self.config.processed_data_file_path, index=False
+                )
+                logger.info(
+                    f"File converted to csv and saved at {self.config.processed_data_file_path}"
+                )
             else:
-                logger.info(f"File already present at path {self.config.processed_data_dir_path}")
+                logger.info(
+                    f"File already present at path {self.config.processed_data_dir_path}"
+                )
 
-            logger.info(f"{'>'*10} Data Ingestion Stage Completed Successfully! {'<'*10}")
+            logger.info(
+                f"{'>'*10} Data Ingestion Stage Completed Successfully! {'<'*10}"
+            )
         except Exception as e:
             raise e
--- src/Phishing_Detector/components/data_transformation.py
+++ src/Phishing_Detector/components/data_transformation.py
@@ -6,11 +6,11 @@ from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
+
 class DataTransformation:
-    
-    def __init__(self, config:DataTransformationConfig):
+    def __init__(self, config: DataTransformationConfig):
         logger.info(f"{'>'*10} Starting Data Transformation Stage! {'<'*10}")
-        
+
         logger.info(f"Getting Configuration for Data Transformation Stage...")
         self.config = config
         logger.info(f"Configurations : {config}")
@@ -23,7 +23,7 @@ class DataTransformation:
         try:
             logger.info(f"Loading data validation report...")
             validation_report = load_json(Path(self.config.data_validation_report))
-            final_validation_status = validation_report['final_validation_status']
+            final_validation_status = validation_report["final_validation_status"]
             logger.info(f"Data Validation Status : {final_validation_status}")
             return final_validation_status
         except Exception as e:
@@ -35,16 +35,22 @@ class DataTransformation:
                 logger.info(f"Reading the validated data...")
                 data = pd.read_csv(self.config.processed_data_file_path)
 
-                logger.info(f"Filling null values using SimpleImputer(strategy = {self.config.param_imputer_strategy})...")
+                logger.info(
+                    f"Filling null values using SimpleImputer(strategy = {self.config.param_imputer_strategy})..."
+                )
                 imputer = SimpleImputer(strategy=self.config.param_imputer_strategy)
                 imputer.fit(data)
                 logger.info(f"Null Values Imputed Successfully...")
                 imputed_data = imputer.transform(data)
                 imputed_df = pd.DataFrame(imputed_data, columns=data.columns)
-                imputed_df.to_csv(self.config.transformed_data_file_path, index = False)
-                logger.info(f"Transformed Data Saved at {self.config.transformed_data_file_path}...")
+                imputed_df.to_csv(self.config.transformed_data_file_path, index=False)
+                logger.info(
+                    f"Transformed Data Saved at {self.config.transformed_data_file_path}..."
+                )
             else:
-                logger.info(f'Data is not validated - Validation Status = {self.get_validation_status()}')
+                logger.info(
+                    f"Data is not validated - Validation Status = {self.get_validation_status()}"
+                )
         except Exception as e:
             raise e
 
@@ -53,28 +59,31 @@ class DataTransformation:
             logger.info(f"Splitting the transformed data into train and test splits...")
 
             data = pd.read_csv(self.config.transformed_data_file_path)
-            X = data.drop(self.config.param_target, axis = 1)
+            X = data.drop(self.config.param_target, axis=1)
             Y = data[self.config.param_target]
-            x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size= self.config.param_test_size, 
-                                                                random_state= self.config.param_random_state)
+            x_train, x_test, y_train, y_test = train_test_split(
+                X,
+                Y,
+                test_size=self.config.param_test_size,
+                random_state=self.config.param_random_state,
+            )
             logger.info(f"Train and test splits created successfully...")
 
-            x_train.to_csv(self.config.x_train_file_path, index = False)
+            x_train.to_csv(self.config.x_train_file_path, index=False)
             logger.info(f"x_train saved at {self.config.x_train_file_path}")
 
-            x_test.to_csv(self.config.x_test_file_path, index = False)
+            x_test.to_csv(self.config.x_test_file_path, index=False)
             logger.info(f"x_test saved at {self.config.x_test_file_path}")
 
-            y_train.to_csv(self.config.y_train_file_path, index = False)
+            y_train.to_csv(self.config.y_train_file_path, index=False)
             logger.info(f"y_train saved at {self.config.y_train_file_path}")
 
-            y_test.to_csv(self.config.y_test_file_path, index = False)
+            y_test.to_csv(self.config.y_test_file_path, index=False)
             logger.info(f"y_test saved at {self.config.y_test_file_path}")
 
-            logger.info(f"{'>'*10} Data Transformation Stage Completed Successfully! {'<'*10}")
+            logger.info(
+                f"{'>'*10} Data Transformation Stage Completed Successfully! {'<'*10}"
+            )
 
         except Exception as e:
             raise e
-
-
-        
